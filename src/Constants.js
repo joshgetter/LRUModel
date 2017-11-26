098044c4f0d3fa2eda85e12364a9a817d0222441
Josh Getter <getterj@mail.gvsu.edu>
2017-11-26 17:30:32 -0500

    Added toolbar and started indexing references

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+//ACTION STRINGS
+const ADD_REFERENCE = 'AddReference';
+const INC_REFERENCE = 'IncReference';
+//STORE ENTRIES
+const REFERENCE_LIST = 'ReferenceList';
+const CURRENT_REFERENCE = 'CurrentReference';
---
+++ src/Models.js
@@ -0,0 +1,8 @@
+//MODELS
+class ProcessReference{
+    index = 0;
+    constructor(process, reference){
+        this.process = process;
+        this.reference = reference;
+    }
+}
