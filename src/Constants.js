00509e1f3cce4988bceb30770b6bb94c15f3cfec
Josh Getter <getterj@mail.gvsu.edu>
2017-11-27 22:24:30 -0500

    PCB and Page Tables added

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,14 @@
 //ACTION STRINGS
 const ADD_REFERENCE = 'AddReference';
 const INC_REFERENCE = 'IncReference';
+const ADD_PAGETABLE = 'AddPageTable';
+const ADD_PCB = 'AddPcb';
+const ADD_FRAME = 'AddFrame';
+const INC_FRAMEREF = "IncFrameRef";
+const ADD_PAGEENTRY = 'AddPageEntry';
 //STORE ENTRIES
 const REFERENCE_LIST = 'ReferenceList';
 const CURRENT_REFERENCE = 'CurrentReference';
+const PAGETABLE_LIST = 'PageTableList';
+const PCB_DICTIONARY = 'PcbDictionary';
+const PHYSICALFRAME_LIST = 'PhysicalFrameList';
--- src/Models.js
+++ src/Models.js
@@ -2,12 +2,52 @@
 class ProcessReference{
     constructor(process, reference){
         this.process = process;
-        this.reference = reference;
+        this._reference = reference;
     }
-    /*set index(index){
-        this._index = index;
+    get reference(){
+        return parseInt(this._reference,2);
     }
-    get index(){
-        return this._index;
-    }*/
+    get binaryReference(){
+        return this._reference;
+    }
+}
+
+class PCB{
+    constructor(process){
+        this.process = process;
+    }
+    get pageTableId(){
+        return this._pageTableId;
+    }
+    set pageTableId(pageTableId){
+        this._pageTableId = pageTableId;
+    }
+}
+
+class PageTable{
+    constructor(process){
+        this.process = process;
+        this.entries = [];
+    }
+    get entries(){
+        return this._entries;
+    }
+    set entries(entries){
+        this._entries = entries
+    }
+}
+
+class PageTableEntry{
+    constructor(page, frame){
+        this.page = page;
+        this.frame = frame;
+    }
+}
+
+class PhysicalEntry{
+    constructor(process, page){
+        this.process = process;
+        this.page = page;
+        this.refs = 1;
+    }
 }
