16179c8887458d0677bf4c1f51af27868547cf6a
Josh Getter <getterj@mail.gvsu.edu>
2017-11-29 19:15:27 -0500

    Free frame list added
    * Reset on new file choice implemented

--- src/Constants.js
+++ src/Constants.js
@@ -4,9 +4,10 @@ const INC_REFERENCE = 'IncReference';
 const ADD_PAGETABLE = 'AddPageTable';
 const ADD_PCB = 'AddPcb';
 const INSERT_FRAME = 'AddFrame';
-const SET_FRAMEREF = "SetFrameRef";
+const SET_FRAMEREF = 'SetFrameRef';
 const ADD_PAGEENTRY = 'AddPageEntry';
 const REMOVE_PAGEENTRY = 'RemovePageEntry';
+const RESET = 'Reset';
 //STORE ENTRIES
 const REFERENCE_LIST = 'ReferenceList';
 const CURRENT_REFERENCE = 'CurrentReference';
